38d2279b1ba4cadecdc1d7bddaec89dd54e49225
jeff.hoang <jeff.hoang@bytedance.com>
2021-10-30 17:00:38 +0800

    feat: Open fragment on btn click

--- app/src/main/java/com/example/cdcdemo/DemoActivity.kt
+++ app/src/main/java/com/example/cdcdemo/DemoActivity.kt
@@ -2,10 +2,38 @@ package com.example.cdcdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.cdcdemo.databinding.ActivityMainBinding
 
 class DemoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.apply {
+            btn1.setOnClickListener {
+                // Show demo
+                supportFragmentManager.apply {
+                    if (findFragmentById(R.id.fragment_container) == null) {
+                        commit {
+                            setReorderingAllowed(true)
+                            add<CurrencyListFragment>(R.id.fragment_container)
+                        }
+                    }
+                }
+            }
+
+            btn2.setOnClickListener {
+                // Sort data
+            }
+        }
     }
 }
